b53e626240f3fca6b465e6aeb04eaa246039af57
Santje777 <s.v.oosterom@gmail.com>
2024-07-12 16:18:31 +0200

    Succesfully requesting fake data

--- Main.py
+++ Main.py
@@ -1,5 +1,14 @@
 from rich import print
 
-
 print("Hello, [red]Susanne[/red]!")
 print("Hello, [bold underline red]Susanne[/bold underline red]!")
+
+import requests
+
+response  = requests.get("https://jsonplaceholder.typicode.com/photos/1")
+
+photo = response.json()
+print(response)
+print(photo)
+print(photo['title'])
+print(photo['thumbnailUrl'])
